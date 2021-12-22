172f70f85af915f87a85d536154b31bb421a57e6
Anders Houbak Kristiansen <palantus@gmail.com>
2021-12-22 15:43:02 +0100

    remove default refs

--- www/libs/refs.mjs
+++ www/libs/refs.mjs
@@ -1,19 +1,9 @@
 export function refToPath(ref){
   switch(ref.type){
-    case "action":
-      return `/action/${ref.id}`
-    case "forumthread":
-      return `/forum/thread/${ref.id}`
-    case "changeset":
-      return `/changeset/${ref.id}`
-    case "support":
-      return `/support/activesupport`
-    case "runbookexec":
-      return `/actions?filter=exec:${ref.id}`
-    case "instance":
-      return `/instance/${ref.id}`
-    case "processes":
-      return `/process`
+
+    // Override this file to include custom ref types
+
+    default:
+      return "";
   }
-  return "";
 }
