3c753e2dc45b8ce23d5611412c85db18e403ac64
Anders Houbak Kristiansen <palantus@gmail.com>
2022-01-28 12:33:33 +0100

    Fix ui

--- ui.mjs
+++ ui.mjs
@@ -7,6 +7,7 @@ export let menu = [
       {title: "Jobs", path: "/setup/jobs"},
       {title: "Logs", path: "/logs"},
       {title: "Mods", path: "/setup/mods"},
+      {title: "Setup", path: "/setup"},
       {title: "Tools", path: "/systemtools"},
       {title: "Users", path: "/setup/users"}
     ]
