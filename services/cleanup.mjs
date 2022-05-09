6ce46ac1d0ba1c53337d07c7bb43adb574fc1052
Anders Houbak Kristiansen <palantus@gmail.com>
2022-05-09 12:38:37 +0200

    Cleanup notifications

--- services/cleanup.mjs
+++ services/cleanup.mjs
@@ -7,13 +7,20 @@ export function startCleanupService(){
 
 function runJob(){
   console.time("Core cleanup completed")
-  let today = new Date()
-  today.setDate(today.getDate()-15)
-  let todayStrISO = today.toISOString()
+  let todayMinus15 = new Date()
+  todayMinus15.setDate(todayMinus15.getDate()-15)
+  let todayMinus15Str = todayMinus15.toISOString()
 
   query.type(LogEntry).tag("logentry").all
-                      .filter(a => a.timestamp < todayStrISO)
+                      .filter(a => a.timestamp < todayMinus15Str)
                       .forEach(a => a.delete());
+
+  let todayMinus30 = new Date()
+  todayMinus30.setDate(todayMinus30.getDate()-30)
+  let todayMinus30Str = todayMinus30.toISOString()
+
+  query.tag("notification").all.filter(n => n.timestamp < todayMinus30Str)
+                               .forEach(n => n.delete())
 
   console.timeEnd("Core cleanup completed")
 }
