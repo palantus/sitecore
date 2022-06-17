180a4d00d7859f8d421985af4adf21bd13fa4beb
Anders Houbak Kristiansen <palantus@gmail.com>
2022-06-17 08:47:48 +0200

    Fix table tools events

--- www/libs/table-tools.mjs
+++ www/libs/table-tools.mjs
@@ -1,34 +1,37 @@
-export function makeRowsSelectable(tableElement, selectionChanged){
-    tableElement.querySelectorAll("tbody tr").forEach(row => {
-        if(row.querySelector(".checkbox"))
-          return;
-          
-        let td = document.createElement("td")
-        td.innerHTML = `<label class="checkbox">
+export function makeRowsSelectable(tableElement, selectionChanged) {
+  tableElement.querySelectorAll("tbody tr").forEach(row => {
+    if (row.querySelector(".checkbox"))
+      return;
+
+    let td = document.createElement("td")
+    td.innerHTML = `<label class="checkbox">
                             <input type="checkbox" class="rowselector"/>
                             <span></span>
                         </label>`
-        row.prepend(td)
+    row.prepend(td)
+  })
+
+  if (!tableElement.classList.contains("rowsselectableenabled")) {
+    tableElement.addEventListener("change", (evt) => {
+      if (evt.target.tagName == "INPUT" && evt.target.classList.contains("rowselector")) {
+        evt.target.parentElement.parentElement.parentElement.classList.toggle("selected")
+        selectionChanged(Array.from(tableElement.querySelectorAll("tbody tr.selected")))
+      }
     })
+    tableElement.classList.add("rowsselectableenabled")
+  }
 
-    if(!tableElement.classList.contains("rowsselectableenabled")){
-      tableElement.addEventListener("change", (evt) => {
-        if(evt.target.tagName == "INPUT" && evt.target.classList.contains("rowselector")){
-          evt.target.parentElement.parentElement.parentElement.classList.toggle("selected")
-        }
-      })
-      tableElement.classList.add("rowsselectableenabled")
-    }
-
-    return {
-        getSelected: () => Array.from(tableElement.querySelectorAll("tbody tr.selected")),
-        clear: () => tableElement.querySelectorAll("tbody tr.selected").forEach(e => {
-            e.classList.toggle("selected")
-            e.querySelector("td:first-child input").checked = false;
-        }),
-        selectAll: () => tableElement.querySelectorAll("tbody tr:not(.selected)").forEach(e => {
-            e.classList.toggle("selected")
-            e.querySelector("td:first-child input").checked = true;
-        })
-    }
+  return {
+    getSelected: () => Array.from(tableElement.querySelectorAll("tbody tr.selected")),
+    clear: () => tableElement.querySelectorAll("tbody tr.selected").forEach(e => {
+      e.classList.toggle("selected")
+      e.querySelector("td:first-child input").checked = false;
+      selectionChanged(Array.from(tableElement.querySelectorAll("tbody tr.selected")))
+    }),
+    selectAll: () => tableElement.querySelectorAll("tbody tr:not(.selected)").forEach(e => {
+      e.classList.toggle("selected")
+      e.querySelector("td:first-child input").checked = true;
+      selectionChanged(Array.from(tableElement.querySelectorAll("tbody tr.selected")))
+    })
+  }
 }
