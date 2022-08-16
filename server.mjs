a8e155542a91e012d677603f686f8865c66c5129
Anders Houbak Kristiansen <palantus@gmail.com>
2022-08-16 12:28:25 +0200

    Add SSL support

--- server.mjs
+++ server.mjs
@@ -1,3 +1,5 @@
+import https from 'https'
+import http from 'http'
 import configLoader from './config/index.mjs';
 import express from 'express';
 import loader from "./loaders/index.mjs"
@@ -5,6 +7,7 @@ import {createServer as createEventServer} from "./services/clientevents.mjs"
 import Entity from 'entitystorage';
 import 'dotenv/config'
 import yargs from 'yargs';
+import fs from 'fs';
 import {resolve} from "path"
 const cliArgs = yargs.argv;
 
@@ -25,13 +28,19 @@ async function startServer() {
     process.exit(-1)
   }
 
-  const server = app.listen(config.port, err => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-    console.log(`Server listening on port: ${config.port}`);
-  });
+  let server; 
+  if(process.env.HTTPS === "TRUE" || process.env.HTTPS === "YES"){
+    if(!process.env.PRIVATE_KEY_FILE || !process.env.CERT_FILE || !process.env.CA_FILE) throw "Missing certificate files (PRIVATE_KEY_FILE, CERT_FILE or CA_FILE) in .env"
+    const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_FILE, 'utf8');
+    const certificate = fs.readFileSync(process.env.CERT_FILE, 'utf8');
+    const ca = fs.readFileSync(process.env.CA_FILE, 'utf8');
+    const credentials = {key: privateKey, cert: certificate, ca};
+    server = https.createServer(credentials, app)
+  } else {
+    server = http.createServer(app)
+  }
+  server.listen(config.port)
+  console.log(`Server listening on port: ${config.port}`);
 
   if(mode != "www"){
     const wsServer = createEventServer();
