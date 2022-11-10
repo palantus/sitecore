8a85693e1830b3b5df729b5dbbf433e67a6fe531
Anders Houbak Kristiansen <palantus@gmail.com>
2022-11-10 08:47:54 +0100

    Fix upload filename encoding

--- loaders/express.mjs
+++ loaders/express.mjs
@@ -30,7 +30,7 @@ export default async ({ app, mode, config }) => {
     //Don't enable cors here, because it can conflict with certain mods (eg. files using webdav-server)
     //app.use(cors());
    
-    app.use(fileUpload());
+    app.use(fileUpload({defParamCharset: "utf8"}));
     app.use(express.json());
     app.use(express.text());
     app.use(express.urlencoded({ extended: false }));
