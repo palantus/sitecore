7f4b2bbe62c00d2f27d79450e82718756097449f
Anders Houbak Kristiansen <palantus@gmail.com>
2023-01-11 16:41:07 +0100

    Add lookup to user for emails

--- models/user.mjs
+++ models/user.mjs
@@ -1,151 +1,156 @@
 "use strict"
 
-import Entity, {query} from "entitystorage"
+import Entity, { query } from "entitystorage"
 import MSUser from "./msuser.mjs"
 import Notification from "./notification.mjs"
-import {pbkdf2Sync} from 'crypto'; 
+import { pbkdf2Sync } from 'crypto';
 import Role from "./role.mjs";
 import { clearUserRoleAndPermissionCache } from "../tools/usercache.mjs";
 
-class User extends Entity{
-    initNew(userId, {name = "", email} = {}){
-        this.id = userId
-        this.name = typeof name === "string" && name.length > 0 ? name : "";
-        if(email && typeof email === "string") this.email = email;
-        this.tag("user")
-    }
+class User extends Entity {
+  initNew(userId, { name = "", email } = {}) {
+    this.id = userId
+    this.name = typeof name === "string" && name.length > 0 ? name : "";
+    if (email && typeof email === "string") this.email = email;
+    this.tag("user")
+  }
 
-    static lookup(id){
-      if(!id) return null;
-      return query.tag("user").prop("id", id).type(User).first
-    }
+  static lookup(id) {
+    if (!id) return null;
+    return query.tag("user").prop("id", id).type(User).first
+  }
 
-    static lookupAdmin(){
-      return User.find("tag:user role.prop:id=admin") || null
-    }
+  static lookupAdmin() {
+    return User.find("tag:user role.prop:id=admin") || null
+  }
 
-    static lookupEmail(email){
-        return MSUser.find(`tag:msuser prop:"email=${email}"`)?.related?.user
-    }
+  static lookupMSEmail(email) {
+    return MSUser.find(`tag:msuser prop:"email=${email}"`)?.related?.user
+  }
 
-    static lookupName(name){
-      if(!name) return null;
-      return query.tag("user").prop("name", name).type(User).first
-    }
+  static lookupEmail(email) {
+    if(!email) return null;
+    return query.tag("user").prop("email", email).first
+  }
 
-    static validateLocalUserId(newId){
-      if(typeof newId !== "string" || !newId) return false;
-      let id = newId.replace(/[^a-zA-Z0-9\-_.]/g, '')
-      if(newId !== id) return false;
-      return true;
-    }
+  static lookupName(name) {
+    if (!name) return null;
+    return query.tag("user").prop("name", name).type(User).first
+  }
 
-    notify(area, message, details){
-      if(this.id == "guest") return;
-      return new Notification(this, area, message, details)
-    }
+  static validateLocalUserId(newId) {
+    if (typeof newId !== "string" || !newId) return false;
+    let id = newId.replace(/[^a-zA-Z0-9\-_.]/g, '')
+    if (newId !== id) return false;
+    return true;
+  }
 
-    setPassword(password){
-      if(password){
-        let salt = global.sitecore.accessTokenSecret.toString('hex'); 
-        this.password = pbkdf2Sync(password, salt,  1000, 64, `sha512`).toString(`hex`);
-      } else {
-        this.password = null;
-      }
-    }
+  notify(area, message, details) {
+    if (this.id == "guest") return;
+    return new Notification(this, area, message, details)
+  }
 
-    validatePassword(possiblePassword){
-      if(!this.password || !possiblePassword) return false;
-      let salt = global.sitecore.accessTokenSecret.toString('hex'); 
-      let hash = pbkdf2Sync(possiblePassword,  salt, 1000, 64, `sha512`).toString(`hex`);
-      return this.password === hash
+  setPassword(password) {
+    if (password) {
+      let salt = global.sitecore.accessTokenSecret.toString('hex');
+      this.password = pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+    } else {
+      this.password = null;
     }
+  }
 
-    static validateEmailAddress(email){
-      if(!email || typeof email !== "string") return false;
-      if(!/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email)) return false;
-      return true;
-    }
+  validatePassword(possiblePassword) {
+    if (!this.password || !possiblePassword) return false;
+    let salt = global.sitecore.accessTokenSecret.toString('hex');
+    let hash = pbkdf2Sync(possiblePassword, salt, 1000, 64, `sha512`).toString(`hex`);
+    return this.password === hash
+  }
 
-    hasPassword(){
-      return this.password ? true : false;
-    }
+  static validateEmailAddress(email) {
+    if (!email || typeof email !== "string") return false;
+    if (!/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email)) return false;
+    return true;
+  }
 
-    get setup(){
-      let setup = this.related.setup
-      if(!setup){
-        setup = new Entity()
-        this.rel(setup, "setup")
-      }
-      return setup
-    }
+  hasPassword() {
+    return this.password ? true : false;
+  }
 
-    get roles(){
-      return this.rels.role?.map(r => r.id)||[]
+  get setup() {
+    let setup = this.related.setup
+    if (!setup) {
+      setup = new Entity()
+      this.rel(setup, "setup")
     }
+    return setup
+  }
 
-    addRole(id){
-      this.rel(Role.lookupOrCreate(id), "role");
-      clearUserRoleAndPermissionCache()
-      return this;
-    }
+  get roles() {
+    return this.rels.role?.map(r => r.id) || []
+  }
 
-    removeRole(id){
-      this.removeRel(Role.lookup(id), "role");
-      clearUserRoleAndPermissionCache()
-      return this;
-    }
+  addRole(id) {
+    this.rel(Role.lookupOrCreate(id), "role");
+    clearUserRoleAndPermissionCache()
+    return this;
+  }
 
-    get permissions(){
-      return [...new Set((this.rels.role?.map(r => r.rels.permission?.map(p => p.id)||[])||[]).flat())]
-    }
+  removeRole(id) {
+    this.removeRel(Role.lookup(id), "role");
+    clearUserRoleAndPermissionCache()
+    return this;
+  }
 
-    get active(){
-      return !this.tags.includes("obsolete")
-    }
+  get permissions() {
+    return [...new Set((this.rels.role?.map(r => r.rels.permission?.map(p => p.id) || []) || []).flat())]
+  }
 
-    deactivate(){
-      this.tag("obsolete")
-      return this;
-    }
+  get active() {
+    return !this.tags.includes("obsolete")
+  }
 
-    activate(){
-      this.removeTag("obsolete")
-      return this;
-    }
+  deactivate() {
+    this.tag("obsolete")
+    return this;
+  }
 
-    static all(){
-      return query.tag("user").type(User).all
-    }
+  activate() {
+    this.removeTag("obsolete")
+    return this;
+  }
 
-    static active(){
-      return query.tag("user").not(query.tag("obsolete")).type(User).all
-    }
+  static all() {
+    return query.tag("user").type(User).all
+  }
 
-    static activeByRole(roleName){
-      let role = Role.lookup(roleName)
-      if(!role) return []
-      return query.tag("user").not(query.tag("obsolete")).type(User).relatedTo(role, "role").all
-    }
+  static active() {
+    return query.tag("user").not(query.tag("obsolete")).type(User).all
+  }
 
-    get msUsers(){
-      return query.type(MSUser).tag("msuser").relatedTo(this, "user").all
-    }
+  static activeByRole(roleName) {
+    let role = Role.lookup(roleName)
+    if (!role) return []
+    return query.tag("user").not(query.tag("obsolete")).type(User).relatedTo(role, "role").all
+  }
 
-    get notifications(){
-      return query.type(Notification).tag("notification").relatedTo(this, "user").all
-    }
+  get msUsers() {
+    return query.type(MSUser).tag("msuser").relatedTo(this, "user").all
+  }
 
-    toObj(){
-        return {
-            id: this.id,
-            name: this.name,
-            msUsers: this.msUsers.map(msu => msu.toObj()),
-            active: !this.tags.includes("obsolete"),
-            virtual: this.tags.includes("virtual"),
-            roles: this.roles
-        }
+  get notifications() {
+    return query.type(Notification).tag("notification").relatedTo(this, "user").all
+  }
+
+  toObj() {
+    return {
+      id: this.id,
+      name: this.name,
+      msUsers: this.msUsers.map(msu => msu.toObj()),
+      active: !this.tags.includes("obsolete"),
+      virtual: this.tags.includes("virtual"),
+      roles: this.roles
     }
+  }
 }
 
 export default User
