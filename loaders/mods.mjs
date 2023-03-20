6796162d6d4d1abc54b33582a0b02c3263c6c181
Anders Houbak Kristiansen <palantus@gmail.com>
2023-03-20 14:56:47 +0100

    Skip mods with unmet dependencies

--- loaders/mods.mjs
+++ loaders/mods.mjs
@@ -2,6 +2,8 @@ import fs from 'fs'
 import glob from 'glob-promise';
 import Entity, { query } from "entitystorage"
 import Mod from '../models/mod.mjs';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
 export default async ({mode}) => {
   let mods = (await new Promise(r => fs.readdir('mods', (err, files) => {if(err) return r([]); r(files)})))
@@ -13,9 +15,19 @@ export default async ({mode}) => {
 
   global.mods = []
   global.modRoutes = "let routes = [];\n"
-  for(let id of mods){
+  modloop: for(let id of mods){
     let mod = Mod.lookupOrCreate(id)
     if(mod.enabled === false) continue;
+    let packageJson
+    try{
+      packageJson = require(`../mods/${id}/package.json`)
+      for(let dep of packageJson.sitecore?.dependencies||[]){
+        if(!mods.includes(dep) || Mod.lookup(dep)?.enabled === false){
+          console.log(`Enabled mod "${id}" depends on mod "${dep}", which is not enabled/installed. "${id}" will not be loaded.`)
+          continue modloop; // Dependency not loaded. Skip.
+        }
+      }
+      } catch(err){}
     let content = await new Promise(r => fs.readFile(`mods/${id}/routes.mjs`, 'utf8', (err, data) => r(err ? "" : data)))
     global.modRoutes += `${content}\n\n`
     mod.hasSetup = new RegExp(`path\:[\ ]*\"\/${id}\/setup\"`).test(content)
