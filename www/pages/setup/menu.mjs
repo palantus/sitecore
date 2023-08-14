3941731da61072e489c0b69cdabb29c8d7c984a8
Anders Kristiansen <palantus@gmail.com>
2023-08-14 10:17:20 +0200

    Allow export of menu

--- www/pages/setup/menu.mjs
+++ www/pages/setup/menu.mjs
@@ -8,6 +8,9 @@ import "/components/field-edit-inline.mjs"
 import Toast from "/components/toast.mjs"
 import {on, off} from "/system/events.mjs"
 import { showDialog, confirmDialog } from "/components/dialog.mjs";
+import { saveFileJSON } from "/libs/file.mjs"
+import { siteTitle } from "/system/core.mjs"
+import { alertDialog } from "../../components/dialog.mjs"
 
 const template = document.createElement('template');
 template.innerHTML = `
@@ -34,6 +37,8 @@ template.innerHTML = `
     <action-bar-item id="refresh-btn">Refresh</action-bar-item>
     <action-bar-item id="regen-menu-btn">Re-generate menu</action-bar-item>
     <action-bar-item id="reset-btn">Reset to defaults</action-bar-item>
+    <action-bar-item id="export-btn">Export</action-bar-item>
+    <action-bar-item id="import-btn">Import</action-bar-item>
   </action-bar>
 
   <div id="container">
@@ -64,6 +69,10 @@ template.innerHTML = `
     <field-component label="Position"><input id="new-path"></input></field-component>
     <field-component label="Target"><input id="new-target"></input></field-component>
   </dialog-component>
+
+  <dialog-component title="Import" id="import-dialog">
+    <input type="file">
+  </dialog-component>
 `;
 
 class Element extends HTMLElement {
@@ -76,16 +85,20 @@ class Element extends HTMLElement {
     this.refreshData = this.refreshData.bind(this);
     this.regenMenu = this.regenMenu.bind(this);
     this.resetAll = this.resetAll.bind(this);
-    this.newItem = this.newItem.bind(this)
+    this.newItem = this.newItem.bind(this);
+    this.export = this.export.bind(this);
+    this.import = this.import.bind(this);
 
     this.shadowRoot.getElementById("new-btn").addEventListener("click", this.newItem)
     this.shadowRoot.getElementById("refresh-btn").addEventListener("click", this.refreshData)
     this.shadowRoot.getElementById("regen-menu-btn").addEventListener("click", this.regenMenu)
     this.shadowRoot.getElementById("reset-btn").addEventListener("click", this.resetAll)
+    this.shadowRoot.getElementById("export-btn").addEventListener("click", this.export);
+    this.shadowRoot.getElementById("import-btn").addEventListener("click", this.import);
   }
 
   async refreshData(){
-    let menu = await api.get("system/menu")
+    let menu = this.menu = await api.get("system/menu")
     this.shadowRoot.getElementById("mods").innerHTML = menu.sort((a, b) => a.path == b.path ? a.title < b.title ? -1 : 1 : a.path < b.path ? -1 : 1).map(mi => `
       <tr class="mi result" data-id="${mi.id}">
         <td><field-edit-inline type="text" patch="system/menu/item/${mi.id}" field="path" value="${mi.path}"></field-edit-inline></td>
@@ -137,6 +150,98 @@ class Element extends HTMLElement {
         this.shadowRoot.querySelectorAll("field-component input").forEach(e => e.value = '')
       }
     })
+  }
+
+  export(){
+    let title = siteTitle().replace(/^\s+|\s+$/g, '') // trim
+                           .replace(/\//g, '-') //Replace / with -
+                           .replace(/[^a-zA-Z0-9 -]/g, '') // remove invalid chars
+                           .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                           .replace(/-+/g, '-'); // collapse dashes
+    saveFileJSON(this.menu, `${title}_menu_${new Date().toISOString()}.json`)
+  }
+
+  import(){
+    let dialog = this.shadowRoot.getElementById("import-dialog")
+    showDialog(dialog, {
+      show: () => {
+        dialog.querySelector("input").focus();
+      },
+      ok: async (val) => {
+        let formData = new FormData();
+        let file = dialog.querySelector("input[type=file]").files[0]
+        if(!file) return;
+        formData.append("file", file);
+        
+        let r = new FileReader();
+        r.onload = async (e) => {
+          try{
+            let menu = JSON.parse(e.target.result);
+            this.importMenu(menu)
+          } catch(err){
+            console.log(err)
+            alertDialog("Could not parse file. Invalid file format.")
+          }
+        };
+        r.readAsText(file);
+      },
+    })
+  }
+
+  async importMenu(rMenu){
+    let changes = []
+    for(let rItem of rMenu){
+      let lItem = null;
+      let potentialLocalItems = this.menu.filter(i => i.target == rItem.target && i.type == rItem.type && i.owner == rItem.owner)
+      if(potentialLocalItems.length > 1){
+        lItem = potentialLocalItems.find(i => i.title == rItem.title)
+      } else {
+        lItem = potentialLocalItems[0] || null
+      }
+      
+      if(!lItem){
+        changes.push({type: "new", rItem})
+        continue;
+      }
+
+      if(rItem.type != lItem.type)
+        changes.push({type: "diff", rItem, lItem, field: "type"})
+
+      if(rItem.title != lItem.title)
+        changes.push({type: "diff", rItem, lItem, field: "title"})
+
+      if(rItem.path != lItem.path)
+        changes.push({type: "diff", rItem, lItem, field: "path"})
+
+      if(rItem.public != lItem.public)
+        changes.push({type: "diff", rItem, lItem, field: "public"})
+
+      if(rItem.hideWhenSignedIn != lItem.hideWhenSignedIn)
+        changes.push({type: "diff", rItem, lItem, field: "hideWhenSignedIn"})
+
+      if(rItem.role != lItem.role)
+        changes.push({type: "diff", rItem, lItem, field: "role"})
+
+      if(rItem.permission != lItem.permission)
+        changes.push({type: "diff", rItem, lItem, field: "permission"})
+
+      if(rItem.hide != lItem.hide)
+        changes.push({type: "diff", rItem, lItem, field: "hide"})
+
+      if(!(await confirmDialog(`
+        Changed items:
+        <br>
+        <pre>${changes.filter(c => c.type == "diff").map(c => `${c.lItem.path}/${c.lItem.title}: Modified ${c.field}`).join("<br>")}</pre>
+        <br>
+        New items:
+        <br>
+        <pre>${changes.filter(c => c.type == "new").map(c => `${c.rItem.path}/${c.rItem.title} => ${c.rItem.target}`).join("<br>")}</pre>
+        `, "Confirm import"))) return;
+
+      alert("continue")
+    }
+
+    console.log(changes)
   }
 
   connectedCallback() {
