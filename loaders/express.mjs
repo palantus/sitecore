50ca3f26d170df473955ccec7ad346f1b844df54
Anders Kristiansen <palantus@gmail.com>
2023-10-18 10:46:12 +0200

    Increase max body size for json parsing

--- loaders/express.mjs
+++ loaders/express.mjs
@@ -32,7 +32,7 @@ export default async ({ app, mode, config }) => {
     //app.use(cors());
    
     app.use(fileUpload({defParamCharset: "utf8"}));
-    app.use(express.json());
+    app.use(express.json({limit: '50mb'}));
     app.use(express.text());
     app.use(express.urlencoded({ extended: false }));
     //app.use(cookieParser());
